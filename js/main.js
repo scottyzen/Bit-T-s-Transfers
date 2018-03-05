4e89a67f3bdc4ff6580034d527839fbe7667ccdc
Scott Kennedy <scottyzen@gmail.com>
2018-03-05 14:04:52 +0000

    From Validation
    Added form validation
    
    Also optimized images

--- js/main.js
+++ js/main.js
@@ -5,28 +5,90 @@ var picker = new Pikaday({
   format: "Do MMMM YYYY"
 });
 
-$("#submit").click(function() {
+// Input fields
+let name = $("#name");
+let email = $("#email");
+let pickup = $("#pickup");
+let dropoff = $("#dropoff");
+let date = $("#datepicker");
+let trip = $("input[name=trip-type]:checked");
+
+function checkIfValid() {
+  // Validate inputs as user types
+  $(".book-online-form").keyup(function (e) {
+    var input = e.target;
+    if (input.value == "") {
+      input.classList = "danger";
+    } else {
+      input.classList = "";
+    }
+  })
+
+  // Final check that everything is valid
+  if ($('input').val() == "" || $('input').hasClass("danger") || !validateEmail(email.val())) {
+    console.log('form is not valid');
+    return false;
+  } else {
+    // if inputs are valid return tre and send form
+    return true
+  }
+}
+
+// Check if Email is Valid
+function validateEmail(email) {
+  var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(String(email).toLowerCase());
+}
+
+
+$("#submit").click(function () {
+  // Check if form fields are valid
+  if (!checkIfValid()) {
+    // If empty on click add the class of danger
+    $('input').each(function () {
+      if (this.value == "") {
+        this.classList = "danger"
+      }
+    });
+
+    // if email is not valid add class of danger
+    if (validateEmail(email.val())) {
+      email.removeClass('danger')
+    } else {
+      console.log('Not a valid email address');
+      email.addClass('danger');
+    }
+
+
+    // Return false and stop form from being sent
+    return false;
+  }
+
+  // make ajax call to send.php
   $.ajax({
     url: "./php/send.php",
     type: "POST",
     data: {
-      name: $("#name").val(),
-      email: $("#email").val(),
-      "pickup-location": $("#pickup").val(),
-      "drop-off-location": $("#dropoff").val(),
-      date: $("#datepicker").val(),
-      "trip-type": $("input[name=trip-type]:checked").val()
+      name: name.val(),
+      email: email.val(),
+      pickup: pickup.val(),
+      dropoff: dropoff.val(),
+      date: date.val(),
+      trip: trip.val()
     },
-    success: function() {
+    success: function () {
+      console.log("Form sending success...");
       $(".book-online-form").html(`
-        <br>
         <div class="form-group">
           <p>Your message has been sent. We will get back to you shortly.</p>
           <h2>Thanks</h2>
+          <br>
         </div>
-        <br>
+        < img src = "images/sent.png">
         `);
     }
   });
   return false;
 });
+
+checkIfValid();
--- php/send.php
+++ php/send.php
@@ -2,25 +2,38 @@
        // from the form
       $name = trim(strip_tags($_POST['name']));
       $email = trim(strip_tags($_POST['email']));
-      $pickupLocation = trim(strip_tags($_POST['pickup-location']));
-      $dropOffLocation = trim(strip_tags($_POST['drop-off-location']));
+      $pickupLocation = trim(strip_tags($_POST['pickup']));
+      $dropOffLocation = trim(strip_tags($_POST['dropoff']));
       $date = trim(strip_tags($_POST['date']));
-      $tripType = trim(strip_tags($_POST['trip-type']));
+      $tripType = trim(strip_tags($_POST['trip']));
 
 
       // set here
       $subject = "Booking from ". $name;
       $to = 'scottyzen@gmail.com';
 
+      // Styled with: https://inlinestyler.torchbox.com/styler/convert/
       $body = <<<HTML
 
-      <h1>Name : $name</h1>
-      <h2>Date : $date</h2>
-      <hr>
-      <h2>Pick up location is : $pickupLocation</h2>
-      <h2>Drop off location is : $dropOffLocation</h2>
-      <hr>
-      <h2><strong>$tripType</strong></h2>
+  <div class="container" style="max-width: 600px;margin: 2% 1%;font-family: &quot;Source Sans Pro&quot;, &quot;Helvetica Neue&quot;, Arial, sans-serif;background: #F2F7FC;border-radius: 5px;color: #2e475f;padding: 10px 20px;font-weight: 200;font-size: 16px">
+    <h1>
+      <strong>$name</strong>
+    </h1>
+    <p>Date:
+      <strong>$date</strong>
+    </p>
+    <hr style="border: 0;height: 1px;background: #b8c7d6; margin: 2% 0"/>
+    <p>Pick up location is:
+      <strong>$pickupLocation</strong>
+    </p>
+    <p>Drop off location is:
+      <strong>$dropOffLocation</strong>
+    </p>
+    <hr style="border: 0;height: 1px;background: #b8c7d6; margin: 2% 0"/>
+    <p>
+      <strong>$tripType</strong>
+    </p>
+  </div>
 
 HTML;
 
@@ -30,6 +43,4 @@ HTML;
       // send the email
       mail($to, $subject, $body, $headers);
 
-      // redirect afterwords, if needed
-      // header('Location: ../index.html');
 ?>
